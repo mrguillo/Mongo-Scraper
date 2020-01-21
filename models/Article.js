573c4a26cc3db0d9643f9a997603e8db0e262bda
Memo Salcedo <guillosc@gmail.com>
2020-01-20 23:23:22 -0600

    adding handlebars

--- models/Article.js
+++ models/Article.js
@@ -1,20 +1,34 @@
+// Require mongoose
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+var Note = require("./Note");
+
+// Create article schema
 var ArticleSchema = new Schema({
     title: {
         type: String,
         required: true
     },
+    summary: {
+        type: String,
+        //    required: true
+    },
     link: {
         type: String,
         required: true
     },
-    note: {
+    saved: {
+        type: Boolean,
+        default: false
+    },
+    notes: [{
         type: Schema.Types.ObjectId,
         ref: "Note"
-    }
+    }]
 });
 
+// Create the Article model with the ArticleSchema
 var Article = mongoose.model("Article", ArticleSchema);
 
+// Export the model
 module.exports = Article;
--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 // SERVER APP
 // ---------------------------------------------------------
 
+// Require all models
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 var moment = require('moment');
 moment().format();
 
@@ -13,7 +15,6 @@ moment().format();
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
 var db = require("./models");
 var PORT = process.env.PORT || 3000;
 
@@ -25,25 +26,50 @@ var app = express();
 // ---------------------------------------------------------
 
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/mongoscraper", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/mongoscraper", {
+    useNewUrlParser: true
+});
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Routes
 // ---------------------------------------------------------
 
 // A GET route for the HOME index
 app.get("/", function (req, res) {
-    res.send(index.html);
+    db.Article.find({
+            "saved": false
+        }, function (error, data) {
+            var object = {
+                article: data
+            };
+            console.log(object);
+        }).then(function (dbArticle) {
+            // res.json(dbArticle);
+            res.render("home", dbArticle)
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
 });
 
 // A GET route for SCRAPING the mashable.com site
 console.log("\n***********************************\n" +
-            "Grabbing every thread title and link\n" +
-            "from www.nytimes.com HOME page:" +
-            "\n***********************************\n");
+    "Grabbing every thread title and link\n" +
+    "from www.nytimes.com HOME page:" +
+    "\n***********************************\n");
 app.get("/scrape", function (req, res) {
     axios.get("http://www.nytimes.com/").then(function (response) {
         var $ = cheerio.load(response.data);
@@ -53,12 +79,13 @@ app.get("/scrape", function (req, res) {
             result.link = "https://www.nytimes.com" + $(this).find("a").attr("href");
             console.log(result);
             db.Article.create(result)
-            .then(function(dbArticle) {
-                console.log(dbArticle);
-            })
-            .catch(function(err) {
-                console.log(err);
-            });
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                    res.json(err);
+                });
         });
         res.send("www.nytimes.com home page succesfuly scraped and saved to MongoDB " + moment().calendar());
     });
@@ -109,7 +136,95 @@ app.post("/articles/:id", function (req, res) {
         });
 });
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// NEW ******************************
+
+// Delete an article
+app.post("/articles/delete/:id", function (req, res) {
+    // Use the article id to find and update its saved boolean
+    Article.findOneAndUpdate({
+            "_id": req.params.id
+        }, {
+            "saved": false,
+            "notes": []
+        })
+        // Execute the above query
+        .exec(function (err, doc) {
+            // Log any errors
+            if (err) {
+                console.log(err);
+            } else {
+                // Or send the document to the browser
+                res.send(doc);
+            }
+        });
+});
+
+
+// Create a new note
+app.post("/notes/save/:id", function(req, res) {
+// Create a new note and pass the req.body to the entry
+var newNote = new Note({
+  body: req.body.text,
+  article: req.params.id
+});
+console.log(req.body)
+// And save the new note the db
+newNote.save(function(error, note) {
+  // Log any errors
+  if (error) {
+    console.log(error);
+  }
+  // Otherwise
+  else {
+    // Use the article id to find and update it's notes
+    Article.findOneAndUpdate({ "_id": req.params.id }, {$push: { "notes": note } })
+    // Execute the above query
+    .exec(function(err) {
+      // Log any errors
+      if (err) {
+        console.log(err);
+        res.send(err);
+      }
+      else {
+        // Or send the note to the browser
+        res.send(note);
+      }
+    });
+  }
+});
+});
+
+// Delete a note
+app.delete("/notes/delete/:note_id/:article_id", function(req, res) {
+// Use the note id to find and delete it
+Note.findOneAndRemove({ "_id": req.params.note_id }, function(err) {
+  // Log any errors
+  if (err) {
+    console.log(err);
+    res.send(err);
+  }
+  else {
+    Article.findOneAndUpdate({ "_id": req.params.article_id }, {$pull: {"notes": req.params.note_id}})
+     // Execute the above query
+      .exec(function(err) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+          res.send(err);
+        }
+        else {
+          // Or send the note to the browser
+          res.send("Note Deleted");
+        }
+      });
+  }
+});
+});
+
+
+// END NEW **************************
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoscraper";
 mongoose.connect(MONGODB_URI);
 
 
@@ -118,4 +233,3 @@ mongoose.connect(MONGODB_URI);
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
 });
-  
